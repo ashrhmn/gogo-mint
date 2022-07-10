8d695e3971052293878aa2118148971306cb1642
ashrhmn <ashrhmn99@gmail.com>
2022-07-11 01:02:45 +0600

    Add token uri route

--- pages/api/v1/tokenUri.ts
+++ pages/api/v1/tokenUri.ts
@@ -1,4 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
+import { getNftMetadata } from "../../../controllers/nft.controller";
 
-export default nextConnect<NextApiRequest, NextApiResponse>();
+export default nextConnect<NextApiRequest, NextApiResponse>().get(
+  getNftMetadata
+);
