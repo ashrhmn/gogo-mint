b67d9d1ed1f46d84f4065990ca960e4849969c7e
ashrhmn <ashrhmn99@gmail.com>
2022-07-11 00:41:31 +0600

    make root env var

--- constants/configuration.ts
+++ constants/configuration.ts
@@ -19,4 +19,6 @@ export const isDevelopment = process.env.NODE_ENV != "production";
 
 export const DISCORD_AUTH_URL = isDevelopment
   ? `https://discord.com/oauth2/authorize?client_id=992820231242268723&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fv1%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify`
-  : `https://discord.com/api/oauth2/authorize?client_id=992820231242268723&redirect_uri=https%3A%2F%2Fgogo-mint.ashrhmn.com%2Fapi%2Fv1%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify`;
+  : `https://discord.com/api/oauth2/authorize?client_id=992820231242268723&redirect_uri=https%3A%2F%2F${
+      process.env.HOST_ROOT || "gogo-mint.ashrhmn.com"
+    }%2Fapi%2Fv1%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify`;
--- constants/tokenUri.ts
+++ constants/tokenUri.ts
@@ -1,2 +1,4 @@
 export const getTokenUri = (nftId: number) =>
-  `https://gogo-mint.ashrhmn.com/api/v1/nft/tokenUri?item=${nftId}`;
+  `https://${
+    process.env.HOST_ROOT || "gogo-mint.ashrhmn.com"
+  }/api/v1/nft/tokenUri?item=${nftId}`;
