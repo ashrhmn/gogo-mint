05f4eda23e1457c33a8f5d931dc66ef537a611d9
ashrhmn <ashrhmn99@gmail.com>
2022-08-23 08:43:02 +0600

    add alchemy on rinkeby

--- constants/RPC_URL.ts
+++ constants/RPC_URL.ts
@@ -1,7 +1,7 @@
 export const RPC_URLS: { [chainId: number]: string } = {
   1: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
   3: `https://ropsten.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
-  4: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
+  4: "https://eth-rinkeby.alchemyapi.io/v2/ih9-sO2b3BLVwkT4G1o2TVeDDuCTgQzw",
   5: `https://eth-goerli.g.alchemy.com/v2/ih9-sO2b3BLVwkT4G1o2TVeDDuCTgQzw` as string,
   42: `https://kovan.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
   56: `https://bsc-dataseed.binance.org/`,
