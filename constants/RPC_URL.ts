833182d93cac3bb14c27e242e31bbe78ba68552a
ashrhmn <ashrhmn99@gmail.com>
2022-08-26 21:10:17 +0600

    add only infura

--- constants/RPC_URL.ts
+++ constants/RPC_URL.ts
@@ -1,8 +1,8 @@
 export const RPC_URLS: { [chainId: number]: string } = {
   1: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
   3: `https://ropsten.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
-  4: "https://eth-rinkeby.alchemyapi.io/v2/ih9-sO2b3BLVwkT4G1o2TVeDDuCTgQzw",
-  5: `https://eth-goerli.g.alchemy.com/v2/ih9-sO2b3BLVwkT4G1o2TVeDDuCTgQzw` as string,
+  4: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
+  5: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
   42: `https://kovan.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
   56: `https://bsc-dataseed.binance.org/`,
   43114: `https://api.avax.network/ext/bc/C/rpc`,
@@ -12,3 +12,17 @@ export const RPC_URLS: { [chainId: number]: string } = {
   43113: `https://api.avax-test.network/ext/bc/C/rpc`,
   137: `https://polygon-rpc.com`,
 };
+// export const RPC_URLS: { [chainId: number]: string } = {
+//   1: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
+//   3: `https://ropsten.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
+//   4: "https://eth-rinkeby.alchemyapi.io/v2/ih9-sO2b3BLVwkT4G1o2TVeDDuCTgQzw",
+//   5: `https://eth-goerli.g.alchemy.com/v2/ih9-sO2b3BLVwkT4G1o2TVeDDuCTgQzw` as string,
+//   42: `https://kovan.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
+//   56: `https://bsc-dataseed.binance.org/`,
+//   43114: `https://api.avax.network/ext/bc/C/rpc`,
+//   97: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+//   80001: `https://matic-mumbai.chainstacklabs.com`,
+//   4002: `https://rpc.testnet.fantom.network`,
+//   43113: `https://api.avax-test.network/ext/bc/C/rpc`,
+//   137: `https://polygon-rpc.com`,
+// };
