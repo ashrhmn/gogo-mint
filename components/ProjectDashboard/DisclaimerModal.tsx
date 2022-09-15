40f0e9748866a346a07016f89d4f127341b981a7
ashrhmn <ashrhmn99@gmail.com>
2022-09-16 01:50:55 +0600

    add terms and conditions docs

--- components/ProjectDashboard/DisclaimerModal.tsx
+++ components/ProjectDashboard/DisclaimerModal.tsx
@@ -1,38 +1,50 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { IS_DISCLAIMER_SHOWN_KEY } from "../../constants/configuration";
 
 const DisclaimerModal = () => {
   const [isModalActive, setIsModalActive] = useState(false);
+  const [isCheckMarked, setIsCheckMarked] = useState(false);
 
   useEffect(() => {
     const isDisclaimerShown = localStorage.getItem(IS_DISCLAIMER_SHOWN_KEY);
-    console.log({ isDisclaimerShown });
     if (!isDisclaimerShown) setIsModalActive(true);
   }, []);
 
   const handleAcceptClick = () => {
+    if (!isCheckMarked) {
+      toast.error("You must check the tickmark to continue");
+      return;
+    }
     setIsModalActive(false);
     localStorage.setItem(IS_DISCLAIMER_SHOWN_KEY, "nQTR3VjtB2KS6EwXVctapnNE");
   };
 
   if (isModalActive)
     return (
-      <div className="text-gray-300 fixed inset-0 bg-black/40 z-50 flex justify-center items-center">
-        <div className="bg-gray-700 max-w-2xl mx-auto p-7 rounded shadow-2xl shadow-gray-800">
-          <h1 className="text-3xl md:text-5xl my-4">Terms and Conditions</h1>
-          <p className="max-h-[70vh] overflow-y-auto">
-            Terms and Conditions in short. Terms and Conditions in short. Terms
-            and Conditions in short. Terms and Conditions in short. Terms and
-            Conditions in short. Terms and Conditions in short. Terms and
-            Conditions in short. Terms and Conditions in short. Terms and
-            Conditions in short. Terms and Conditions in short.{" "}
-            <Link href={"#"} passHref>
-              <a className="text-blue-500">
-                Detailed Terms and Conditions Page (Link to a PDF maybe)
-              </a>
-            </Link>
-          </p>
+      <div className="text-gray-300 fixed inset-0 bg-black/80 z-50 flex justify-center items-center">
+        <div className="bg-gray-800 max-w-2xl mx-auto p-7 rounded-xl shadow-2xl shadow-gray-800/30">
+          <h1 className="text-3xl md:text-4xl my-4 font-medium">
+            Terms and Conditions
+          </h1>
+          <div className="flex items-center gap-3">
+            <input
+              type="checkbox"
+              checked={isCheckMarked}
+              onChange={(e) => setIsCheckMarked(e.target.checked)}
+            />
+            <p className="max-h-[70vh] overflow-y-auto">
+              By clicking “Accept” I agree to the{" "}
+              <Link href={"/docs/1. Terms of Service.pdf"} passHref>
+                <a className="text-blue-500">Terms of Service</a>
+              </Link>{" "}
+              and{" "}
+              <Link href={"/docs/2. Privacy Policy.pdf"} passHref>
+                <a className="text-blue-500">Privacy Policy</a>
+              </Link>
+            </p>
+          </div>
           <div className="flex flex-col s360:flex-row justify-end gap-4 my-3">
             <Link href={"/"} passHref>
               <button className="bg-red-600 hover:bg-red-700 transition-colors rounded p-2 w-full s360:w-28">
