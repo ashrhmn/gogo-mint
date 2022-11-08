ef305111a82d3a636d9921ae9a1bcfa582490d09
ashrhmn <ashrhmn99@gmail.com>
2022-11-08 16:36:51 +0600

    add royalty snippets and remove deprecated networks

--- constants/RPC_URL.ts
+++ constants/RPC_URL.ts
@@ -1,9 +1,6 @@
 export const RPC_URLS: { [chainId: number]: string } = {
   1: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
-  3: `https://ropsten.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
-  4: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
   5: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
-  42: `https://kovan.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
   56: `https://bsc-dataseed.binance.org/`,
   43114: `https://api.avax.network/ext/bc/C/rpc`,
   97: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
