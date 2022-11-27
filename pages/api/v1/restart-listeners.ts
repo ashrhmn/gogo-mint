584a99a6800e95b9f751549c0483926d2b77f60e
ashrhmn <ashrhmn99@gmail.com>
2022-11-27 20:30:20 +0600

    update compose file

--- pages/api/v1/restart-listeners.ts
+++ pages/api/v1/restart-listeners.ts
@@ -5,7 +5,7 @@ import nextConnect from "next-connect";
 export default nextConnect<NextApiRequest, NextApiResponse>().get(
   async (req, res) => {
     await axios
-      .get(`http://54.153.49.223:4200/restart`)
+      .get(`https://dev.listener.hydromint.xyz/restart`)
       .then((res) => res.data)
       .then((data) => {
         return res.json(data);
