5ec5a634e81ec3953f0efd5b92e7c7a807d68ad4
ashrhmn <ashrhmn99@gmail.com>
2023-01-22 07:26:15 +0600

    remove header logger

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,5 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   if (!request.url.match("_next"))
     console.log(request.nextUrl.pathname + request.nextUrl.search);
-
-  console.log(request.headers.values());
-
   return NextResponse.next();
 }
