5a31f5efa44ed83e8d1cb625c39d1ec7b1971edf
ashrhmn <ashrhmn99@gmail.com>
2023-01-22 07:16:41 +0600

    add geo ip on logger

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,8 @@ export function middleware(request: NextRequest) {
   if (!request.url.match("_next"))
     console.log(request.nextUrl.pathname + request.nextUrl.search);
 
+  if (!!request.geo) console.log(JSON.stringify(request.geo));
+  if (!!request.ip) console.log(JSON.stringify(request.ip));
+
   return NextResponse.next();
 }
