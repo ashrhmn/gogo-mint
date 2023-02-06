011fe3e78d3efa8caad566d6710b26cbe139142a
ashrhmn <ashrhmn99@gmail.com>
2023-02-06 12:18:03 +0600

    add infura key as optional

--- constants/RPC_URL.ts
+++ constants/RPC_URL.ts
@@ -1,6 +1,10 @@
 export const RPC_URLS: { [chainId: number]: string } = {
-  1: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
-  5: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` as string,
+  1: `https://mainnet.infura.io/v3/${
+    process.env.NEXT_PUBLIC_INFURA_KEY || "523cf9d9caac42d08bf74057b72a8218"
+  }` as string,
+  5: `https://goerli.infura.io/v3/${
+    process.env.NEXT_PUBLIC_INFURA_KEY || "523cf9d9caac42d08bf74057b72a8218"
+  }` as string,
   56: `https://bsc-dataseed.binance.org/`,
   43114: `https://api.avax.network/ext/bc/C/rpc`,
   97: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
